7bcc509590247eb193d8f52abd88401acd1a7335
Kayla San Filippo <kaylasanfilippo@MacBook-Pro-2.local>
2020-08-15 22:21:19 -0700

    fun

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -47,7 +47,7 @@ var yay = ["../images/yay2.jpg", '../images/yay2.jpg', '../images/yay3.jpg', '..
 
 
 function countdown() {
-    var i = 80;
+    var i = 5;
 
     // This block will be executed 100 times.
     setInterval(function () {
