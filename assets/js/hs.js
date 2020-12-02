c6bd026df73b3d03fcd18509c6ebed241c30f225
Kayla San Filippo <kaylasanfilippo@MacBook-Pro-2.local>
2020-12-02 09:30:49 -0800

    css and results handling

--- assets/js/hs.js
+++ assets/js/hs.js
@@ -1,19 +1,30 @@
 
-var userName = localStorage.getItem('User')
-var score = localStorage.getItem('Score')
-var time = localStorage.getItem('Time')
+// var userName = localStorage.getItem('User')
+// var score = localStorage.getItem('Score')
+// var time = localStorage.getItem('Time')
 var listie = document.getElementById('listie')
-console.log(userName)
+var listMe = []
+var topScore = localStorage.getItem("topScores");
+console.log("this be topscore " + topScore)
+listMe.push(topScore)
+console.log(listMe)
+console.log(`this is list meeee '${listMe}'`)
 
-for (let i = 0; i < localStorage.length; i++) {
+for (let i = 0; i < listMe.length; i++) {
+ console.log(`this is iiii ${listMe[i]} and more ${listMe[i][3]}`)
+
+  
+  
     var list = `
    <div class="list-group  pb-3">
    <a href="#" class="list-group-item list-group-item-action list-group-item-success text-justify">
-   <div class="lead">User: ${userName}</div>
-   <div class="lead">Score: ${score}</span></div>
-   <div class="lead">Seconds Remaining: ${time} </span></div>
+   <div class="lead">User: ${listMe[i]}</div>
+   <div class="lead">Score: ${listMe[i][4]}/10</span></div>
+   <div class="lead">Seconds Remaining: ${listMe[i][2]} </span></div>
    </a>
    </div>
    `
     listie.innerHTML = list
+
+
 }
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -5,6 +5,8 @@ var seconds = document.querySelector('#time-remaining')
 var timesUp = document.querySelector('#timesUp')
 var display = document.querySelector('#remaining')
 //  QUIZ VARIABLES
+var currentQuestion = document.getElementById('question-title').innerHTML
+
 var questionTitle = document.getElementById('question-title')
 var cardQuestion = document.getElementById('card-question')
 var q0 = document.getElementById('q0')
@@ -27,7 +29,11 @@ var yey = document.getElementById('winning')
 var userBtn = document.getElementById('button-addon2')
 var userInput = document.getElementById('userInput')
 var navie = document.getElementById('nav')
+var currScore = document.querySelector('#currentScore')
 
+// var currTime = document.getElementById('time-remaining').innerHTML
+var topScoreArr = []
+var userTime 
 
 
 
@@ -44,14 +50,15 @@ var ranPic = ["../images/boo1.jpg", '../images/boo2.jpg', '../images/boo3.jpg', 
 ]
 var yay = ["../images/yay2.jpg", '../images/yay2.jpg', '../images/yay3.jpg', '../images/yay4.jpg', '../images/yay5.jpg', '../images/yay6.jpg', '../images/yay7.jpg'
 ]
+var greenPortal = ['../images/portal.PNG']
 
 
 function countdown() {
-    var i = 5;
-
+    var i = 137;
+   
     // This block will be executed 100 times.
     setInterval(function () {
-        if (i == 0) {
+        if(currQ > 10){
             clearInterval(this);
             display.setAttribute('style', "display: none");
             timesUp.setAttribute('style', "display: show");
@@ -59,6 +66,16 @@ function countdown() {
             document.getElementById('total').setAttribute('style', "display: show")
             navie.setAttribute('style', "display:show")
 
+        }
+        if (i == 0) {
+           
+            userTime = seconds.innerHTML
+            clearInterval(this);
+            display.setAttribute('style', "display: none");
+            timesUp.setAttribute('style', "display: show");
+            document.getElementById('cardDisplay').setAttribute('style', "display: none")
+            document.getElementById('total').setAttribute('style', "display: show")
+            navie.setAttribute('style', "display:show")
             //display score, option for highscores, play again. 
 
         }
@@ -139,15 +156,19 @@ function getQuiz() {
     displayNext()
 }
 
-
-
-
 var answerQuestion;
 function checkValue(value) {
     answerQuestion = value
 }
 
 function displayNext() {
+    //if there are no more questions, dont throw error. 
+if(currQ == undefined){
+    console.log('No error, no more questions')
+
+}else{
+    
+    //create if statement for when there is no longer an upcoming Q
     questionTitle.innerHTML = currQ.title
     cardQuestion.innerHTML = currQ.question
     q0.innerHTML = currQ.choices[0]
@@ -155,37 +176,86 @@ function displayNext() {
     q2.innerHTML = currQ.choices[2]
     q3.innerHTML = currQ.choices[3]
 }
+}
+
+
+
 
 userBtn.addEventListener('click', function (e) {
     e.preventDefault()
-    console.log(total)
-    localStorage.setItem("User", userInput.value);
-    localStorage.setItem("Score", total);
+   
+//         var prevArr = localStorage.getItem("topScores");
+
+//    if(prevArr != null){
+//     console.log(`this is prev arrrr` + prevArr)
+//     topScoreArr.push(prevArr)
+//    }    
+   
+    var percentage =  function(x){
+      var dec =  x/10
+    var scoress = dec * 100
+      return scoress
+
+    }
+// var nueUser= {'UserName':userInput.value,'Score':total,'EndTime':userTime}
+var alsoNue=[`Username: ${userInput.value}`,`Score: ${total}`,`Seconds Left: ${userTime}`,`Percentage: ${percentage(total).toFixed(1)}`]
+
+     //push new user into array
+    topScoreArr.push(alsoNue)
+    console.log(`this is user array after pushing new user ${topScoreArr}`)
+
+   
+    for (let i = 0; i < topScoreArr.length; i++){
+    console.log(`LOOOOPPPP ${topScoreArr[i]}`)
+    var list = `
+
+    <div class="card mb-4 col-sm-12 col-lg-8 col-md-8 p-0 mx-auto" style="background-color:#2d2d2d">
+
+    <h5 class="card-title p-1" style="background-color:#1f1f1f">Results</h5>
+    <p class="lead card-text">${topScoreArr[i][0]}</p>
+    <p class="lead card-text">${topScoreArr[i][1]}/10</span></p>
+    <p class="lead card-text">${topScoreArr[i][2]} </span></p>
+    <p class="lead card-text mb-3" style="color:red">${topScoreArr[i][3]} </span></p>
+    </a>
+    </div>
+    `}
+    currScore.innerHTML = list
+    //grab previous user from storage and add to object & check to see which one has the highest scores
+    // localStorage.setItem("topScores", JSON.stringify(topScoreArr));
+    
+//call function to sort through array and put remaining users into local storage for displaying top 3. 
+
+    // localStorage.setItem("Score", total);
     userBtn.setAttribute('style', 'display: none')
     userInput.setAttribute('style', 'display: none')
     document.getElementById('goSee').setAttribute('style', 'display: show')
 
+
 })
 
+
 subButton.addEventListener("click", function (e) {
+    e.preventDefault()
     if (answerQuestion === undefined) {
         answerQuestion = 0
     }
 
-    var currentQuestion = parseInt(document.getElementById('question-title').innerHTML)
-    console.log(currentQuestion)
+    currentQuestion = parseInt(document.getElementById('question-title').innerHTML)
+    // console.log(currentQuestion)
 
-    if (currentQuestion == 10) {
+    if (currentQuestion === 10) {
         document.getElementById('cardDisplay').setAttribute('style', "display: none")
+        document.getElementById('timesUp').setAttribute('style', "display: none")
         document.getElementById('total').setAttribute('style', "display: show")
+
         // document.getElementById('userTotal').innerHTML = total
         navie.setAttribute('style', "display:show")
         display.setAttribute('style', "display: none");
 
-
-        var endTime = seconds.innerHTML
-        localStorage.setItem("Time", endTime);
-        // console.log(endTime)
+        userTime = seconds.innerHTML
+    
+        // localStorage.setItem("Time", endTime);
+        console.log("this is end time " + userTime)
     }
 
     if (answerQuestion == questions[(currentQuestion - 1)].answer) {
@@ -193,7 +263,7 @@ subButton.addEventListener("click", function (e) {
         win.setAttribute('style', 'display: show')
         yey.setAttribute('src', randomPic(yay))
         total++
-        console.log('you got it right!' + total)
+        // console.log('you got it right!' + total)
 
         setTimeout(function () {
             win.setAttribute('style', 'display: none')
@@ -204,13 +274,13 @@ subButton.addEventListener("click", function (e) {
 
 
     else if (answerQuestion != questions[(currentQuestion - 1).answer]) {
-        console.log('nah that aint right')
+        // console.log('nah that aint right')
         display.setAttribute('style', "display: none");
         document.getElementById('cardDisplay').setAttribute('style', "display: none")
         lose.setAttribute('style', 'display: show')
         setTimeout(function () {
             var i = seconds.innerHTML;
-            console.log(i)
+            // console.log(i)
             display.setAttribute('style', "display: show");
             document.getElementById('cardDisplay').setAttribute('style', "display: show")
             lose.setAttribute('style', 'display: none')
@@ -288,3 +358,36 @@ function getNext() {
 
 init()
 
+
+
+
+
+// var timeEl = document.querySelector(".time");
+// var mainEl = document.getElementById("main");
+
+// var secondsLeft = 10;
+
+// function setTime() {
+//   var timerInterval = setInterval(function() {
+//     secondsLeft--;
+//     timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+
+//     if(secondsLeft === 0) {
+//       clearInterval(timerInterval);
+//       sendMessage();
+//     }
+
+//   }, 1000);
+// }
+
+// function sendMessage() {
+//   timeEl.textContent = " ";
+
+//   var imgEl = document.createElement("img");
+
+//   imgEl.setAttribute("src", "images/image_1.jpg");
+//   mainEl.appendChild(imgEl);
+
+// }
+
+// setTime();
